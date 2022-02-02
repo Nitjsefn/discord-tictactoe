b7e26da6ef26717329ea59d2de8f3d48e31f0ee5
Nitjsefn <28672240+Nitjsefn@users.noreply.github.com>
2022-02-02 14:25:58 +0100

    created discord client object and added callback funcs to events

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const prefix = process.env.BOT_PREFIX;
 const token = process.env.BOT_TOKEN;
 const { log } = console;
 const { Client, Intents, MessageEmbed, MessageAttachment, MessageActionRow, MessageButton } = require('discord.js');
+
+bot.login(token);
+bot.once("ready", () => { log("Bot connected"); BOT_NAME = bot.user.username; });
+bot.on("messageCreate", msg => messageCreateAndUpdateMethod(msg));
+bot.on("messageUpdate", (old_msg, msg) => messageCreateAndUpdateMethod(msg));
+bot.on('interactionCreate', interaction => {
+	if(interaction.isButton) reactToButton(interaction);
+});
